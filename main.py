b88b4023e87b9a4529249e480e44c569dcd3db1e
hjcheong12 <108015557+hjcheong12@users.noreply.github.com>
2023-05-08 21:53:17 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <title>LearnUs</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+    <nav>
+        <div class="container">
+            <h1><a href="./index.html">LearnUs</a></h1>
+            <ul>
+                <li><a href="#">전문과정</a></li>
+                <li><a href="#">공개과정</a></li>
+                <li><a href="#">국제과정</a></li>
+                <li><a href="#">학위과정</a></li>
+                <li><a href="#">이벤트</a></li>
+                <li><a href="#">이용안내</a></li>
+            </ul>
+        </div>
+    </nav>
+    <header>
+        <div class="container">
+            <h2>10대를 위한 LearnUs 시리즈</h2>
+            <p>학교 밖에서 배우는 공부</p>
+        </div>
+    </header>
+    <section>
+        <div class="container">
+            <article></article>
+        </div>
+    </section>
+</body>
+</html>
